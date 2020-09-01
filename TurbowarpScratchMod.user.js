4a538e70eda9462448c2929ae248420eb4954046
YOYITsM3M8 <49005044+YOYITsM3M8@users.noreply.github.com>
2020-09-01 17:47:01 -0400

    Update TurbowarpScratchMod.user.js

--- TurbowarpScratchMod.user.js
+++ TurbowarpScratchMod.user.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name         Turbowarp Scratch Mod
 // @namespace    YOYITsM3M8
-// @version      1.0
+// @version      2.0
 // @description  adds a "View in Turbowarp" button next to see inside
 // @author       YOYITsM3M8
 // @match        https://scratch.mit.edu/projects/*
-// @run-at       document-end
+// @homepageURL  https://github.com/yoyitsm3m8/userscripts
+// @supportURL   https://github.com/yoyitsm3m8/userscripts/issues/new/choose
+// @updateURL    https://yoyitsm3m8.github.io/userscripts/TurbowarpScratchMod.user.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
+    console.log("Turbowarp Scratch Mod (c) 2020 YOYITsM3M8 has loaded");
     var current_url = document.location; // get url
     var id = current_url.href.split('/')[4]; // get project id from url
     function check() { // check if page loaded
@@ -35,5 +38,42 @@
         }
         catch(err) {setTimeout(check, 300);/* try again in 300 milliseconds */}
     }
-    check() //run check at script start
+    function check2() { // check if page loaded
+        const el = document.querySelector('.menu-bar_main-menu_3wjWH');
+        try {
+            if (el.classList[0] == "menu-bar_main-menu_3wjWH") {
+                console.log("element load")
+                var turbobtn = document.createElement("div")
+                turbobtn.classList.add("menu-bar_menu-bar-item_oLDa-")
+                var btninside = document.createElement("span");
+                btninside.classList.add("button_outlined-button_1bS__");
+                btninside.classList.add("menu-bar_menu-bar-button_3IDN0");
+                btninside.classList.add("community-button_community-button_2Lo_g");
+                btninside.style="background-color: hsla(0, 100%, 65%, 1);";
+                btninside.role = "button"
+                var btncontent = document.createElement("div");
+                btncontent.classList.add("button_content_3jdgj");
+                var span = document.createElement("a");
+                span.href="https://turbowarp.org/editor.html#"+id;
+                span.style="color: white;font-size: 0.75rem;font-weight: bold;";
+                var node = document.createTextNode("Edit in Turbowarp");
+                span.appendChild(node);
+                btncontent.appendChild(span);
+                btninside.appendChild(btncontent);
+                turbobtn.appendChild(btninside);
+                el.appendChild(turbobtn);
+                console.log(turbobtn);
+            } else {
+                setTimeout(check2, 300); // try again in 300 milliseconds
+            }
+        }
+        catch(err) {setTimeout(check2, 300);/* try again in 300 milliseconds */}
+    }
+
+    if (document.location.href.split("/")[5] == "editor") {
+        check2() //run check at script start
+    } else {
+        check()
+    }
+
 })();
