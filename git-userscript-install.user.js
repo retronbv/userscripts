a681846bae69c777abb864df458d70ba8caf8c59
retron <retronbv@gmail.com>
2022-04-05 15:58:47 -0400

    v3.0

--- git-userscript-install.user.js
+++ git-userscript-install.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         Github Userscript Install
-// @namespace    http://github.com/YOYITsM3M8/userscripts/git-userscript-install.user.js/
-// @version      2.1
+// @namespace    http://github.com/retronbv/userscripts/git-userscript-install.user.js/
+// @version      3.0
 // @description  adds a userscript install button to quickly add the file to your userscript browser extention!
-// @author       YOYITsM3M8
+// @author       retronbv
 // @match        https://github.com/*.user.js
-// @updateURL    https://yoyitsm3m8/userscripts/git-userscript-install.user.js
+// @downloadURL    https://retronbv.github.io/userscripts/git-userscript-install.user.js
+// @updateURL    https://retronbv.github.io/userscripts/git-userscript-install.user.js
 // @grant        none
 // ==/UserScript==
 (function() {
-    var buttons = document.getElementsByClassName("BtnGroup")[0];
-    var raw = buttons.childNodes[2]
+    var buttons = document.getElementsByClassName("BtnGroup")[1];
+    var raw = buttons.childNodes[1]
     raw.innerText = "Install Userscript";
 })();
